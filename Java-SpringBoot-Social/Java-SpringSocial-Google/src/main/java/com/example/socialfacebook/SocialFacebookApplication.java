857f52008235dc3005e4ebbd77c0a93a42768f87
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2019-05-02 10:54:46 -0400

    agregar los proyectos de linkedin y google

---
+++ Java-SpringBoot-Social/Java-SpringSocial-Google/src/main/java/com/example/socialfacebook/SocialFacebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialfacebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialFacebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialFacebookApplication.class, args);
+    }
+
+}
---
+++ Java-SpringBoot-Social/Java-SpringSocial-Linkedin/src/main/java/com/example/socialfacebook/controller/MainController.java
@@ -0,0 +1,46 @@
+package com.example.socialfacebook.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {  
+//    private Facebook facebook;
+//    private ConnectionRepository connectionRepository;
+//
+//    @Autowired
+//    public MainController(Facebook facebook, ConnectionRepository connectionRepository) {
+//        this.facebook = facebook;
+//        this.connectionRepository = connectionRepository;
+//    }
+//
+//    @RequestMapping(value = "feed", method = RequestMethod.GET)
+//    public String feed(Model model) {
+//
+//        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+//            return "redirect:/connect/facebook";
+//        }
+//
+//        User userProfile = facebook.userOperations().getUserProfile();
+//        model.addAttribute("userProfile", userProfile);
+//        PagedList<Post> userFeed = facebook.feedOperations().getFeed();
+//        model.addAttribute("userFeed", userFeed);
+//        return "feed";
+//    }
+
+    @RequestMapping(
+        value = "/",
+        method = RequestMethod.GET
+    )
+    public String index() {
+        return "index";
+    }
+}
